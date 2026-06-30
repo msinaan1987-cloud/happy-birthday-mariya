4864cf7cfe5c4afe41c6eeed7f5f01ea974fa116
msinaan1987-cloud <msinaan1987@gmail.com>
2026-06-30 17:02:48 +0530

    Update animation.js

---
+++ js/animation.js
@@ -0,0 +1,71 @@
+const revealBtn = document.getElementById("revealBtn");
+
+const magicContainer = document.getElementById("magicContainer");
+
+const magicPath = document.getElementById("magicPath");
+
+const footprints = document.getElementById("footprints");
+
+const finalMessage = document.getElementById("finalMessage");
+
+const subText = document.getElementById("subText");
+
+const startBtn = document.getElementById("startBtn");
+
+revealBtn.addEventListener("click", () => {
+
+    revealBtn.style.display = "none";
+
+    magicContainer.style.display = "block";
+
+    footprints.style.display = "block";
+
+    magicPath.style.animation = "drawLine 5s linear forwards";
+
+    setTimeout(() => {
+
+        magicContainer.style.opacity = "0";
+
+        footprints.style.opacity = "0";
+
+    }, 5200);
+
+    setTimeout(() => {
+
+        finalMessage.style.opacity = "1";
+
+        finalMessage.style.pointerEvents = "auto";
+
+        finalMessage.style.animation = "fadeIn 1.5s forwards";
+
+    }, 6200);
+
+    setTimeout(() => {
+
+        subText.style.animation = "fadeIn 1.5s forwards";
+
+    }, 7000);
+
+    setTimeout(() => {
+
+        startBtn.style.display = "block";
+
+        startBtn.style.animation = "buttonGlow 2s infinite";
+
+    }, 8200);
+
+});
+
+startBtn.addEventListener("click", () => {
+
+    document.body.style.transition = "1.2s";
+
+    document.body.style.opacity = "0";
+
+    setTimeout(() => {
+
+        window.location.href = "pages/hogwarts.html";
+
+    }, 1200);
+
+});
