c02d91f9b3a72cd025722ee0441752c6e7ca68c0
msinaan1987-cloud <msinaan1987@gmail.com>
2026-07-02 13:40:11 +0530

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,1 +1,15 @@
+const video=document.getElementById("loveVideo");
 
+const button=document.getElementById("proposalBtn");
+
+video.onended=function(){
+
+button.style.display="inline-block";
+
+}
+
+button.onclick=function(){
+
+window.location.href="proposal.html";
+
+}
