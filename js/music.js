ea671a427f366d9cbdc8b8dc94ed8af25d4b2ee5
msinaan1987-cloud <msinaan1987@gmail.com>
2026-07-18 11:12:04 +0530

    Add background music playback functionality

--- js/music.js
+++ js/music.js
@@ -1,1 +1,25 @@
+const bgMusic = document.getElementById("bgMusic");
 
+if (bgMusic) {
+
+    bgMusic.volume = 0.30;
+
+    const playPromise = bgMusic.play();
+
+    if (playPromise !== undefined) {
+
+        playPromise.catch(() => {
+
+            document.addEventListener(
+                "click",
+                () => {
+                    bgMusic.play();
+                },
+                { once: true }
+            );
+
+        });
+
+    }
+
+}
