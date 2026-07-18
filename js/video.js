f06069e68b14b8ea9d2b5d697e8e0a5aae06a633
msinaan1987-cloud <msinaan1987@gmail.com>
2026-07-18 11:15:38 +0530

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,19 +1,9 @@
-const music = document.getElementById("bgMusic");
-
-if(music){
-
-music.pause();
-
-}
-
 const video=document.getElementById("loveVideo");
 
 const button=document.getElementById("proposalBtn");
 
 video.onended=function(){
 
-  music.play().catch(() => {});
-  
 button.style.display="inline-block";
 
 }
